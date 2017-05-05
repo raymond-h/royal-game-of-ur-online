4074cdecb4c4fffeaa6038ccb1c0342d747c2394
Raymond Hammarling <raymond@hammarlings.se>
2017-05-06 00:12:56 +0200

    Run deepstream, use Koa

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: node subfolder/index.js web
+web: node subfolder/index.js
+deepstream: ./node_modules/.bin/deepstream start -c ./ds-conf/config.yml
--- subfolder/index.js
+++ subfolder/index.js
@@ -1,6 +1,19 @@
-const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const static = require('koa-static');
+const proxy = require('koa-proxy');
 
-http.createServer((req, res) => {
-	res.end('What the dude!!!');
-})
-.listen(process.env.PORT);
+const app = new Koa();
+
+const router = new Router();
+
+router.use('/static', static('./static'));
+
+// router.get('/deepstream', proxy({
+// 	url: 'http://localhost:6020/deepstream'
+// }));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(process.env.PORT);
